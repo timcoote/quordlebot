218ff4f63467a8f916e3ff5f0ae924ee7922c523
Tim Coote <tim+github.com@coote.org>
2022-03-21 15:26:35 +0000

    three

--- x.py
+++ x.py
@@ -2,24 +2,24 @@ import enchant
 
 dictionary = enchant.Dict ('en_US')
 #dictionary = enchant.Dict ('en_GB')
-must = 'sai'
-may = 'qopdhjzxvbnm'
+must = 'aicn'
+may = 'qopdhjzxvm'
 letters = must + may
 for a in letters:
-    if a in '': continue
+    if a in 'c': continue
     for b in letters:
         if b not in 'a': continue
         for c in letters:
-            if c not in 's': continue
+            if c in 'i': continue
             for d in letters:
-                if d in '': continue
+                if d not in 'i': continue
                 for e in letters:
-                    if e  in '': continue
+                    if e  in 'n': continue
                     candidate = f"{a}{b}{c}{d}{e}"
                     if dictionary.check (candidate):
                         truth =[c in candidate for c in must]
                         if all (truth):
-                            #print (truth, candidate)
-                            print (candidate)
+                            print (truth, candidate)
+                            #print (candidate)
 
 
